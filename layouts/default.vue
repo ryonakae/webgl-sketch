a7a3a58d8d8f3faf1b6b680d712e3eaf21ca18fd
Ryo Nakae <ryo.n.1129@gmail.com>
2018-04-17 21:32:05 +0900

    update

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <header>
+      <h1><nuxt-link to="/">WebGL Study</nuxt-link></h1>
+    </header>
+
+    <nuxt />
+
+    <footer>
+      <a href="https://twitter.com/ryo_dg" target="_blank">&copy;Ryo Nakae</a>
+    </footer>
+  </div>
+</template>
